139ff681232589176f1d97b58f0d1458da26cadb
fluffy <fluffy@beesbuzz.biz>
2021-05-10 01:05:31 -0700

    Get it actually working

--- DDRuino.ino
+++ DDRuino.ino
@@ -1,4 +1,14 @@
+/*@ file DDRunino.ino
+ * 
+ * Simple Arduino sketch to run a DDR pad as a keyboard
+ * 
+ * https://git.beesbuzz.biz/fluffy/DDRuino/
+ */
+
 #include <Keyboard.h>
+
+// Uncomment this to enable the serial debugger to determine the pin mappings
+//#define DEBUG
 
 struct Input {
     int pin;
@@ -7,19 +17,27 @@ struct Input {
 };
 
 Input mapping[] = {
-    { 1, KEY_RETURN },
-    { 2, KEY_ESC },
-    { 3, KEY_UP_ARROW },
-    { 4, KEY_DOWN_ARROW },
-    { 5, KEY_RIGHT_ARROW },
-    { 6, KEY_LEFT_ARROW },
+    { 4, KEY_RETURN },
+    { 5, KEY_ESC },
+    { 6, KEY_UP_ARROW },
+    { 7, KEY_DOWN_ARROW },
+    { 8, KEY_RIGHT_ARROW },
+    { 9, KEY_LEFT_ARROW },
 };
 
 void setup()
 {
+    Serial.begin(9600);
+
+#ifdef DEBUG
+    for (int i = 0; i < 12; i++) {
+      pinMode(i, INPUT_PULLUP);
+    }
+#endif
+
     for (auto i : mapping) {
         pinMode(i.pin, INPUT_PULLUP);
-        i.state = false;
+        i.state = HIGH;
     }
 
     Keyboard.begin();
@@ -27,8 +45,15 @@ void setup()
 
 void loop()
 {
+#ifdef DEBUG
+    for (int i = 0; i < 12; i++) {
+      Serial.print(!digitalRead(i) ? "0123456789abcdef"[i] : ' ');
+    }
+    Serial.print('\n');
+#endif
+  
     for (auto& i : mapping) {
-        bool val = digitalRead(i.pin);
+        bool val = !digitalRead(i.pin);
         if (val != i.state) {
             if (val) {
                 // TODO debounce
